5bc604b94b07ec9b45985e1a34bb31419fd8b78d
Nagendra <nagendra@Nagendras-MacBook-Pro.local>
2022-10-10 18:12:50 +0530

    added new api changes

--- pipelines.py
+++ pipelines.py
@@ -1,123 +1,67 @@
 from constants import STORE_WH_MAP
 
 
-def group_autocomplete_stage(
-    keyword="", store_id="", platform="pos", order_type="retail", skip=0, limit=10
-):
-    search_terms_len = len(keyword.split(" "))
-    SEARCH_PIPE = []
-    if search_terms_len == 1:
-        SEARCH_PIPE = {
-            "$search": {
-                "compound": {
-                    "should": [
-                        {
-                            "autocomplete": {
-                                "query": keyword,
-                                "path": "name",
-                            },
-                        },
-                        {
-                            "autocomplete": {
-                                "query": keyword,
-                                "path": "barcode",
-                            },
-                        },
-                    ],
-                },
+GROUP_ADDITIONAL_STAGE = [
+ {'$project': {'id': '$product_id', 'inv_qty': 1, '_id': 0, 'group_id': 1}},
+ {'$sort': {'inv_qty': -1}},
+ {
+        '$group': {
+            '_id': '$group_id', 
+            'count': {
+                '$sum': 1
             }
         }
-    else:
-        SEARCH_PIPE = {
-            "$search": {
-                "text": {
-                    "query": keyword,
-                    "path": "name",
-                },
+    }, {
+        '$match': {
+            '_id': {
+                '$ne': None
             }
         }
+    }, {
+        '$project': {
+            'id': '$_id', 
+            '_id': 0
+        }
+    }]
 
-    is_mall = "0"
-    if order_type == "mall":
-        is_mall = "1"
-    match_filter = {"is_mall": is_mall}
-
-    if platform == "app":
-        match_filter["sale_app"] = "1"
-    else:
-        match_filter["sale_pos"] = "1"
-
-    if is_mall == "1":
-        wh_id = STORE_WH_MAP.get(store_id)
-        PIPELINE = SEARCH_PIPE + [
-            {"$match": match_filter},
-            {
-                "$lookup": {
-                    "from": "product_warehouse_stocks",
-                    "localField": "id",
-                    "foreignField": "product_id",
-                    "as": "data",
-                    "pipeline": [
-                        {"$match": {"warehouse_id": wh_id}},
-                        {"$project": {"warehouse_id": 1, "stock": 1}},
-                    ],
+def listing_pipeline(skip, limit, match_filter):
+    return [
+    {
+        '$match': match_filter
+    }, {
+        '$project': {
+            '_id': 0, 
+            'group_id': 1
+        }
+    }, {
+        '$group': {
+            '_id': '$group_id', 
+            'count': {
+                '$sum': 1
+            }
+        }
+    }, {
+        '$project': {
+            'id': '$_id', 
+            '_id': 0
+        }
+    }, {
+        '$facet': {
+            'total': [
+                {
+                    '$count': 'count'
                 }
-            },
-            {"$project": {"_id": 0, "id": 1, "stock": {"$first": "$data.stock"}}},
-            # {"$match": {"stock": {"$gt": 0}}},
-            {"$sort": {"stock": -1}},
-            {
-                "$facet": {
-                    "total": [{"$count": "count"}],
-                    "data": [{"$skip": skip}, {"$limit": limit}],
+            ], 
+            'data': [
+                {
+                    '$skip': skip
+                }, {
+                    '$limit': limit
                 }
-            },
-        ]
-
-    else:
-
-        PIPELINE = [
-            {"$search": {"autocomplete": {"query": keyword, "path": "name"}}},
-            {
-                "$lookup": {
-                    "from": "search_products",
-                    "localField": "id",
-                    "foreignField": "group_id",
-                    "as": "product_data",
-                    "pipeline": [
-                        {
-                            "$lookup": {
-                                "from": "product_store",
-                                "localField": "id",
-                                "foreignField": "product_id",
-                                "as": "store_data",
-                                "pipeline": [{"$match": {"store_id": store_id}}],
-                            }
-                        }
-                    ],
-                }
-            },
-            {
-                "$match": {
-                    "product_data": {"$ne": []},
-                    "product_data.is_mall": "0",
-                    "product_data.sale_app": "1",
-                    "product_data.store_data": {"$ne": []},
-                }
-            },
-            {"$sort": {"product_data.store_data.inv_qty": -1}},
-            {"$project": {"id": 1, "_id": 0, "name": 1}},
-            {
-                "$facet": {
-                    "total": [{"$count": "count"}],
-                    "data": [{"$skip": skip}, {"$limit": limit}],
-                }
-            },
-        ]
-
-    print(PIPELINE)
-    return PIPELINE
-
+            ]
+        }
+    }
+]
 
 def get_boosting_stage(
     keyword="", store_id="", platform="pos", order_type="retail", skip=0, limit=10
@@ -234,8 +178,8 @@ def get_boosting_stage(
                 }
             },
         ]
-    print(PIPELINE)
     return PIPELINE
+
 
 
 def get_pipeline_from_sharded_collection(
@@ -330,5 +274,22 @@ def get_pipeline_from_sharded_collection(
                 }
             },
         ]
-    print(PIPELINE)
+
     return PIPELINE
+
+def get_search_pipeline(keyword, store_id, platform, order_type, skip, limit):
+    pipe_line = []
+    if order_type == 'mall':
+        pipe_line = get_boosting_stage(keyword, store_id, platform, order_type, skip, limit)
+    else:
+        pipe_line = get_pipeline_from_sharded_collection(keyword, store_id, platform, order_type, skip, limit)
+    return pipe_line
+
+
+def group_autocomplete_stage(
+    keyword="", store_id="", platform="pos", order_type="retail", skip=0, limit=10
+):
+    PIPELINE = get_search_pipeline(keyword, store_id, platform, order_type, skip, limit)
+    NEW_GROUP_PIPELINE = PIPELINE[:-3] + GROUP_ADDITIONAL_STAGE + [PIPELINE[-1]]
+    print(NEW_GROUP_PIPELINE)
+    return NEW_GROUP_PIPELINE
